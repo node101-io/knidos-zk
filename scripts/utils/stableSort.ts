9626266ef66e850770aa09beed56aa905a964c02
tunntun <mervehtnozcan@gmail.com>
2026-01-28 17:17:35 +0300

    add stable sort

---
+++ scripts/utils/stableSort.ts
@@ -0,0 +1,22 @@
+import type { NormalizedFills } from "../types"
+
+export function stableSort(fills:NormalizedFills[]): NormalizedFills {
+  if (fills.length === 0)
+    return [];
+  return fills
+      .map((fill, index) => ({ fill, index }))
+      .sort((a, b) => {
+
+        if (a.fill.time !== b.fill.time)
+          return a.fill.time - b.fill.time;
+        // if the time is exactly the same we check Trade ID
+        if (a.fill.tid !== b.fill.tid)
+          return a.fill.tid - b.fill.tid;
+        // if trade ID also the same we check Order ID
+        if (a.fill.oid !== b.fill.oid)
+          return a.fill.oid - b.fill.oid;
+
+        return a.index - b.index;
+      })
+      .map((  { fill }  ) => fill);
+}
